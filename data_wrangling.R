1d7d68a392a5c36ffbfc67bb2d4f88b6c7ac3c58
MatteoLarrode <matteo.larrode@orange.fr>
2023-04-08 21:37:07 -0400

    new dataframe for analysis 1

--- data_wrangling.R
+++ data_wrangling.R
@@ -5,10 +5,13 @@ library(tidyverse)
 
 df <- read_csv("data/final_data.csv")
 
+df_noNA <- read_csv("data/final_data.csv")%>% 
+  drop_na()
+
 
 #Dataframe for freedom of discussion, Internet penetration & Level of democracy
 
-df_reduced1 <- df %>% 
+df_reduced1 <- df_noNA %>% 
   select(Economy, value_2021_perc, v2x_libdem, v2cldiscm, v2cldiscw)%>%
   #threshold of 0.5
   mutate(is_dem = v2x_libdem >= 0.5)
