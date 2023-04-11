a0d585747ded20b3206ce7ee63344a29b2fba2e7
MatteoLarrode <matteo.larrode@orange.fr>
2023-04-10 21:04:56 -0400

    class 10/04

--- models.R
+++ models.R
@@ -2,5 +2,5 @@
 
 # Effect of Internet penetration on freedom of speech for women w/ interaction term
 
-model1 <- lm(data = df_reduced,  v2cldiscw ~ value_2021_perc * v2x_libdem)
+model1 <- lm(data = df_reduced,  v2cldiscw ~ value_2021_perc * is_dem)
 summary(model1)
